f012a7083b4168f0b08383b83a6bddbbc144c6e9
francomendezok <125481413+francomendezok@users.noreply.github.com.>
2025-02-15 21:40:42 +0000

    first access form

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+
+export const metadata: Metadata = {
+  title: "The Album Over Vinyl Experience",
+  description: "Listen to music like the good old days",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
